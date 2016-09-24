09cb5f9d9d223d23191f2cc06ef48a7c27f61cf8
Sruti <sruti@gatech.edu>
2016-09-24 11:43:45 -0400

    xml for login, register

---
+++ app/src/main/java/com/ballersmeet/sruti/ballersmeet/control/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.ballersmeet.sruti.ballersmeet.control;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.ballersmeet.sruti.ballersmeet.R;
+
+/**
+ * Created by Sruti on 9/24/16.
+ */
+public class RegisterActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void handleCancelClick(View view) {
+    }
+
+    public void handleRegisterClick(View view) {
+    }
+}
