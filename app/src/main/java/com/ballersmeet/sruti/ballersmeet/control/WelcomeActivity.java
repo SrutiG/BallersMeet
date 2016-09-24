b8abe9fc2e49e2158f83f812609571a4abd58e61
Sruti <sruti@gatech.edu>
2016-09-24 13:03:43 -0400

    added homePageActivity, expandable grid view, made changes to xml, added png file

---
+++ app/src/main/java/com/ballersmeet/sruti/ballersmeet/control/WelcomeActivity.java
@@ -0,0 +1,19 @@
+package com.ballersmeet.sruti.ballersmeet.control;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ballersmeet.sruti.ballersmeet.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void handleRegisterClick(View view) {
+    }
+}
