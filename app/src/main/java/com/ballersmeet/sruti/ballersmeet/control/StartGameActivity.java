6ef8661837f08e472473e138d22bd50991055972
Anna Herrera <aherrera8@gatech.edu>
2016-09-25 00:09:16 -0400

    maps things

--- app/src/main/java/com/ballersmeet/sruti/ballersmeet/control/StartGameActivity.java
+++ app/src/main/java/com/ballersmeet/sruti/ballersmeet/control/StartGameActivity.java
@@ -1,16 +1,22 @@
 package com.ballersmeet.sruti.ballersmeet.control;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.ballersmeet.sruti.ballersmeet.R;
 import com.ballersmeet.sruti.ballersmeet.model.Athlete;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.Serializable;
 
-public class StartGameActivity extends AppCompatActivity {
+public class StartGameActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private Athlete athlete;
 
@@ -19,6 +25,9 @@ public class StartGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
         athlete = (Athlete) getIntent().getExtras().getSerializable("athlete");
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
 
@@ -38,5 +47,12 @@ public class StartGameActivity extends AppCompatActivity {
         Intent startView = new Intent(this, StartGameActivity.class);
         startView.putExtra("athlete", (Serializable) athlete);
         startActivity(startView);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
     }
 }
